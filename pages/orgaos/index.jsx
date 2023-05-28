d150b0ebf362746ecbfa811e50a7537f34cf3f15
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-28 20:47:26 -0300

    comecando orgãos

--- pages/orgaos/index.jsx
+++ pages/orgaos/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Pagina from "../../components/Pagina";
 
-const index = () => {
+const index = ({ orgaos }) => {
   return <Pagina>index</Pagina>;
 };
 
 export default index;
+export async function getServerSideProps(context) {
+  const resultado = await apiDeputados.get(`/orgaos?itens=100`);
+  const orgaos = await resultado.data.dados;
+  return {
+    props: { orgaos }, // will be passed to the page component as props
+  };
+}
