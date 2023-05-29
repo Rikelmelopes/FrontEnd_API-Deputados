5042199f7780c555fde9d8c914d7542452676f97
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-28 22:31:16 -0300

    ajustando teste

--- pages/teste.jsx
+++ pages/teste.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import apiDeputados from "../services/apiDeputados";
 
 const teste = () => {
-  const [teste, setTeste] = useState([]);
-
   useEffect(() => {
     apiDeputados.get(`/deputados/220593/discursos`).then((resultado) => {
       console.log(resultado.data.dados);
