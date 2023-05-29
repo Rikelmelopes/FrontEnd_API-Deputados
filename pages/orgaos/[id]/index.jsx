db6116058ddf846a886f93d9e7942711556f469d
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-28 22:27:15 -0300

    informaçoes do orgãos

--- pages/orgaos/[id]/index.jsx
+++ pages/orgaos/[id]/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Pagina from "../../../components/Pagina";
-import { Card, Col, Nav, Row, Table } from "react-bootstrap";
+import { Card, Col, ListGroup, Nav, Row, Table } from "react-bootstrap";
 import apiDeputados from "../../../services/apiDeputados";
 import Link from "next/link";
 
@@ -23,6 +23,23 @@ const index = ({ orgao }) => {
             </Nav.Item>
           </Nav>
           <h3>Informações</h3>
+          <ListGroup>
+            <ListGroup.Item action variant="dark">
+              Nome: <strong>{orgao.nome}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item action variant="dark">
+              Apelido: <strong>{orgao.apelido}</strong>
+            </ListGroup.Item>{" "}
+            <ListGroup.Item action variant="dark">
+              Sala: <strong>{orgao.sala}</strong>
+            </ListGroup.Item>{" "}
+            <ListGroup.Item action variant="dark">
+              Sigla: <strong>{orgao.sigla}</strong>
+            </ListGroup.Item>{" "}
+            <ListGroup.Item action variant="dark">
+              Tipo: <strong>{orgao.tipoOrgao}</strong>
+            </ListGroup.Item>{" "}
+          </ListGroup>
         </Col>
       </Row>
     </Pagina>
