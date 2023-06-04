db77af3827baed6de5ffa442a37d16eba3d35f9c
JoaoeduLS <joaoeduardo.lsilva@mgail.com>
2023-06-04 16:35:37 -0300

    adicionado o boxShadow e o transition para coloca os efeitos nos cards

--- pages/deputados/index.jsx
+++ pages/deputados/index.jsx
@@ -56,7 +56,20 @@ const Index = ({ deputados }) => {
                     textAlign: "center",
                   }}
                 >
-                  <Card border="success" style={{ width: "18rem" }}>
+                  <Card
+                    border="success"
+                    style={{
+                      width: "18rem",
+                      boxShadow: "0 17px 10px rgba(0, 0, 0, 0.3)",
+                      transition: "transform 0.3s ease",
+                    }}
+                    onMouseEnter={(e) => {
+                      e.currentTarget.style.transform = "scale(1.05)";
+                    }}
+                    onMouseLeave={(e) => {
+                      e.currentTarget.style.transform = "scale(1)";
+                    }}
+                  >
                     <Card.Img variant="top" src={item.urlFoto} />
                     <Card.Body>
                       <Card.Title>{item.nome}</Card.Title>
