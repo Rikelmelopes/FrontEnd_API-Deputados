e3c7f9f1e71d61e3a38b574bca515ce3f6cccdfa
MarkMaciel <marcos.maciel2002@hotmail.com>
2023-06-25 18:13:54 -0300

    Tela de loading feita

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,26 @@
+import Router from "next/router";
+import React, { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+  return <>{loading ? <Loading /> : <Component {...pageProps} />}</>;
 }
